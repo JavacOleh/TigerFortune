609eed60160b994167ac13df63e3c1eeb6842683
Oleh <olehvasylevych1337@gmail.com>
2025-09-03 16:21:04 +0300

    Fixed: FATAL EXCEPTION: pool-3-thread-4                                                                                                     Process: com.tigerfortune, PID: 9029                                                                                                     java.util.ConcurrentModificationException                                                                                                     	at java.util.ArrayList.forEach(ArrayList.java:1615)                                                                                                     	at com.tigerfortune.engine.addition.TigerAddition.lambda$onUpdate$7(TigerAddition.java:95)                                                                                                     	at com.tigerfortune.engine.addition.TigerAddition.$r8$lambda$xh_C-zusSnIJNTnNH8eVGImw-e8(Unknown Source:0)                                                                                                     	at com.tigerfortune.engine.addition.TigerAddition$$ExternalSyntheticLambda8.run(D8$$SyntheticClass:0)                                                                                                     	at java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1156)                                                                                                     	at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:651)                                                                                                     	at java.lang.Thread.run(Thread.java:1119)

--- app/src/main/java/com/tigerfortune/dto/level/levels/Level1.java
+++ app/src/main/java/com/tigerfortune/dto/level/levels/Level1.java
@@ -25,14 +25,14 @@ public class Level1 implements Runnable {
         levelHandler.addDecorate(0, 0, widthPx / 2, heightPx/ 2, R.drawable.kust);
         levelHandler.addDecorate(0, 0, widthPx / 2, heightPx/ 2, R.drawable.kust);
 
-//        drawable = levelHandler.activityLevel.getResources().getDrawable(R.drawable.brick);
-//        widthPx = drawable.getIntrinsicWidth();
-//        heightPx = drawable.getIntrinsicHeight();
-//        levelHandler.addObstacle(300, 50, 300, 120, R.drawable.brick);
-//        levelHandler.addObstacle(430, 100, 300, 120, R.drawable.brick);
-//        levelHandler.addObstacle(560, 150, 300, 120, R.drawable.brick);
-//        levelHandler.addObstacle(680, 200, 300, 120, R.drawable.brick);
-        levelHandler.addSnakeEnemy(300, 0, 300, 250, R.drawable.snake, null, null);
+        drawable = levelHandler.levelActivity.getResources().getDrawable(R.drawable.brick);
+        widthPx = drawable.getIntrinsicWidth();
+        heightPx = drawable.getIntrinsicHeight();
+        levelHandler.addObstacle(300, 50, 300, 120, R.drawable.brick);
+        levelHandler.addObstacle(430, 100, 300, 120, R.drawable.brick);
+        levelHandler.addObstacle(560, 150, 300, 120, R.drawable.brick);
+        levelHandler.addObstacle(680, 200, 300, 120, R.drawable.brick);
+        //levelHandler.addSnakeEnemy(300, 0, 300, 250, R.drawable.snake, null, null);
 
         levelHandler.addEbnutsaGolovojEl(780, 200, 300, 120);
 
--- app/src/main/java/com/tigerfortune/engine/addition/TigerAddition.java
+++ app/src/main/java/com/tigerfortune/engine/addition/TigerAddition.java
@@ -5,6 +5,7 @@ import static com.tigerfortune.dto.StaticData.speed;
 import static com.tigerfortune.other.util.UiUtil.mainThread;
 
 import android.util.Log;
+import android.view.View;
 
 import com.tigerfortune.dto.tigr.Tiger;
 import com.tigerfortune.engine.Engine;
@@ -18,6 +19,8 @@ import com.tigerfortune.other.user.UserService;
 import com.tigerfortune.other.util.ConcurrentUtil;
 import com.tigerfortune.other.util.DoOnce;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -90,16 +93,23 @@ public class TigerAddition {
             });
 
             executor.execute(() -> {
-                synchronized (this) {
-                    do {
-                        collectable.forEach(view -> {
-                            var tigerCollectAddition = TigerCollectAddition.getInstance(tiger);
-                            tigerCollectAddition.view = view;
-                            tigerCollectAddition.run();
-                        });
-                        ConcurrentUtil.sleep(Engine.waitFor);
-                    } while (true);
-                }
+                do {
+                    List<View> copy;
+                    synchronized (this) {
+                        copy = new ArrayList<>(collectable);
+                    }
+                    copy.forEach(view -> {
+                        var tigerCollectAddition = TigerCollectAddition.getInstance(tiger);
+                        tigerCollectAddition.view = view;
+                        tigerCollectAddition.run();
+                    });
+//                        collectable.forEach(view -> {
+//                            var tigerCollectAddition = TigerCollectAddition.getInstance(tiger);
+//                            tigerCollectAddition.view = view;
+//                            tigerCollectAddition.run();
+//                        });
+                    ConcurrentUtil.sleep(Engine.waitFor);
+                } while (true);
             });
 
             executor.execute(() -> {
